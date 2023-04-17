84936b90da6e46c077e8d55448375766fb86be9c
Chachacharlie <TCCA_EDNS@hotmai.com>
2023-04-16 22:08:51 -0600

    Cambios al Readme: Agregar UserFriendly

--- src/pages/api/dailyArtworkAPI.js
+++ src/pages/api/dailyArtworkAPI.js
@@ -1,7 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import math from "mathjs"
-
 export default function handler(req, res) {
     let dia = new Date().toLocaleDateString()
     
